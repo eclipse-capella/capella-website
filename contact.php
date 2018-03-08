e966600778a3a28dbadacf02f17117b5cb6c1374
lchituc <loredana.chituc@obeo.fr>
2018-03-08 17:49:09 +0100

    Improve SEO : title and main content by adding MBSE tool words
    Change-Id: I69a31fd3c85b50ab794714b972da466ce4d3af59
    Signed-off-by: lchituc <loredana.chituc@obeo.fr>

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <link rel="icon" type="image/png" href="images/favicon.ico">
-        <title>Capella - Contact</title>
+        <title>Capella MBSE Tool - Contact</title>
         <meta name="description" content="">
         <meta name="keywords" content="">
         <meta name="author" content="">
